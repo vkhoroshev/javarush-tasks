01a24030f885375b867b5459b4b9a116bf735ce2
Vadim Khoroshev <vs.khoroshev@gmail.com>
2021-08-27 20:26:29 +0300

    Решить задачу 01-01-02-01

---
+++ src/quest01/level01/lesson02/task01/Main.java
@@ -0,0 +1,7 @@
+package quest01.level01.lesson02.task01;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Я думаю, быть программистом - это круто");
+    }
+}
