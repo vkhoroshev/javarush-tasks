ce400b550201498f66acec427b8be0d36fd69640
Vadim Khoroshev <vs.khoroshev@gmail.com>
2021-08-28 14:31:29 +0300

    Решить задачу 01-01-07-01

---
+++ src/quest01/level01/lesson07/task01/Main.java
@@ -0,0 +1,16 @@
+package quest01.level01.lesson07.task01;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 12;
+
+        // y = x * y;
+        y = x + y;
+        x = y - x;
+        y = y - x;
+
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
