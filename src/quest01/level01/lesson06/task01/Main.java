e1f4c5abee587c28daffff3b1147ddafeecb7fc3
Vadim Khoroshev <vs.khoroshev@gmail.com>
2021-08-28 14:21:26 +0300

    Решить задачу 01-01-06-01

---
+++ src/quest01/level01/lesson06/task01/Main.java
@@ -0,0 +1,16 @@
+package quest01.level01.lesson06.task01;
+
+public class Main {
+    public static void main(String[] args) {
+        // String s = "Merry";
+        // System.out.println("Christmas");
+        // System.out.println("Merry New");
+        // System.out.println("weekend");
+        System.out.print("Happy New");
+        // System.out.println("weekend");
+        // System.out.println(s);
+        // System.out.print("Merry New!");
+        System.out.print(" ");
+        System.out.println("Year");
+    }
+}
