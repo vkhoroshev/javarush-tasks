16cd19ce0fd3e57ff81d9d2b6a555def392d1988
Vadim Khoroshev <vs.khoroshev@gmail.com>
2021-09-01 16:08:07 +0300

    Решить задачу 01-02-04-02

---
+++ src/quest01/level02/lesson04/task02/Main.java
@@ -0,0 +1,9 @@
+package quest01.level02.lesson04.task02;
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 546;
+        int lastDigit = number % 10;
+        System.out.println(lastDigit);
+    }
+}
