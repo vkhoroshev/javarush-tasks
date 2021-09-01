551b709265e02378a55b358b6c25e942eadb07bc
Vadim Khoroshev <vs.khoroshev@gmail.com>
2021-09-01 16:10:29 +0300

    Решить задачу 01-02-04-03

---
+++ src/quest01/level02/lesson04/task03/Main.java
@@ -0,0 +1,13 @@
+package quest01.level02.lesson04.task03;
+
+public class Main {
+    public static void main(String[] args) {
+        int six = 6;
+
+        six++;
+        six++;
+        six++;
+
+        System.out.println(six);
+    }
+}
