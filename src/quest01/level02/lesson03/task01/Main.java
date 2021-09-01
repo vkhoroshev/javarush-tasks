a28795dd30e11511e4047dcdfd2e13f6c007caf9
Vadim Khoroshev <vs.khoroshev@gmail.com>
2021-09-01 16:01:47 +0300

    Решить задачу 01-02-03-01

---
+++ src/quest01/level02/lesson03/task01/Main.java
@@ -0,0 +1,10 @@
+package quest01.level02.lesson03.task01;
+
+public class Main {
+    public static void main(String[] args) {
+        int intValue = 1;
+        int numericValue = 2;
+        int size = 3;
+        int number = 4;
+    }
+}
