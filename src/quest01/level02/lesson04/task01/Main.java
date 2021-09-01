1438a96ad4580147e821687dd1ea7da7e3fa69d7
Vadim Khoroshev <vs.khoroshev@gmail.com>
2021-09-01 16:05:24 +0300

    Решить задачу 01-02-04-01

---
+++ src/quest01/level02/lesson04/task01/Main.java
@@ -0,0 +1,9 @@
+package quest01.level02.lesson04.task01;
+
+public class Main {
+    public static void main(String[] args) {
+        int centimeters = 243;
+        int meters = centimeters / 100;
+        System.out.println(meters);
+    }
+}
