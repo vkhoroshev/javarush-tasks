11a61896b4b8191a91723f3f27afdf9de4636756
Vadim Khoroshev <vs.khoroshev@gmail.com>
2021-09-01 16:16:50 +0300

    Решить задачу 01-02-05-01

---
+++ src/quest01/level02/lesson05/task01/Main.java
@@ -0,0 +1,10 @@
+package quest01.level02.lesson05.task01;
+
+public class Main {
+    public static void main(String[] args) {
+        String word = "1";
+        String phrase = "2";
+        String line = "3";
+        String text = "4";
+    }
+}
