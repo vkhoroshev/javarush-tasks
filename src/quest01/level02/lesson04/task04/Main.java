efe49570f80bad9367a4daeb1576a09f2a58a07a
Vadim Khoroshev <vs.khoroshev@gmail.com>
2021-09-01 16:12:17 +0300

    Решить задачу 01-02-04-04

---
+++ src/quest01/level02/lesson04/task04/Main.java
@@ -0,0 +1,19 @@
+package quest01.level02.lesson04.task04;
+
+public class Main {
+    public static void main(String[] args) {
+        int toothCounter = 32;
+
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+
+        System.out.println(toothCounter);
+    }
+}
