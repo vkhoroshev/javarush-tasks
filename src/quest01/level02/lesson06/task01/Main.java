9d2bd98c6d82de4cb5af36c24f5462c89801f30c
Vadim Khoroshev <vs.khoroshev@gmail.com>
2021-09-02 11:08:57 +0300

    Решить задачу 01-02-06-01

---
+++ src/quest01/level02/lesson06/task01/Main.java
@@ -0,0 +1,12 @@
+package quest01.level02.lesson06.task01;
+
+public class Main {
+    public static void main(String[] args) {
+        String s1 = "Do";
+        String s2 = "It";
+        String s3 = "Just";
+
+        String tagline = "" + s3 + s1 + s2;
+        System.out.println(tagline);
+    }
+}
