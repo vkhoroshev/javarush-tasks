747da365474ac942d9096d7a81531aea60d2a67e
Vadim Khoroshev <vs.khoroshev@gmail.com>
2021-09-07 17:34:46 +0300

    Решить задачу 01-02-06-02

---
+++ src/quest01/level02/lesson06/task02/Main.java
@@ -0,0 +1,9 @@
+package quest01.level02.lesson06.task02;
+
+public class Main {
+    public static void main(String[] args) {
+        String emptiness = "пустота";
+        String fullness = "" + emptiness + " " + emptiness + " " + emptiness;
+        System.out.println(fullness);
+    }
+}
