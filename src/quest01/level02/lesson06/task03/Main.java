83053117af292fee5bd90701ca0b67e446252623
Vadim Khoroshev <vs.khoroshev@gmail.com>
2021-09-16 15:01:47 +0300

    Решить задачу 01-02-06-03

---
+++ src/quest01/level02/lesson06/task03/Main.java
@@ -0,0 +1,12 @@
+package quest01.level02.lesson06.task03;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 4;
+        int z = 0;
+
+        String digits = x + y + "" + z;
+        System.out.println(digits);
+    }
+}
