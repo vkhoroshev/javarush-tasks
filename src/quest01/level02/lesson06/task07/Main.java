a7ddef1ad8eda1bf058c57aadc84d635932d2e59
Vadim Khoroshev <vs.khoroshev@gmail.com>
2021-09-20 10:38:32 +0300

    Решить задачу 01-02-06-07

---
+++ src/quest01/level02/lesson06/task07/Main.java
@@ -0,0 +1,12 @@
+package quest01.level02.lesson06.task07;
+
+public class Main {
+    public static void main(String[] args) {
+        String caps = "if I type in caps ";
+        String usa = "сша";
+
+        System.out.println(usa.toUpperCase());
+        System.out.println("Винни Пух".toUpperCase());
+        System.out.println((caps + "they know I mean business").toUpperCase());
+    }
+}
