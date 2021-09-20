13a6be8fd93e300ed4d75cf04dd622dc38259604
Vadim Khoroshev <vs.khoroshev@gmail.com>
2021-09-20 10:33:46 +0300

    Решить задачу 01-02-06-05

---
+++ src/quest01/level02/lesson06/task05/Main.java
@@ -0,0 +1,11 @@
+package quest01.level02.lesson06.task05;
+
+public class Main {
+    public static void main(String[] args) {
+        String emptyString = "";
+
+        System.out.println(emptyString.length());
+        System.out.println("Gomu Gomu no Bazooka!".length());
+        System.out.println((emptyString + 2 + 2 + "22").length());
+    }
+}
