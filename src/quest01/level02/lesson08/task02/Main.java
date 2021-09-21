6138ef7a6fbe247ccaec993fa8591ed3e0d38d21
Vadim Khoroshev <vs.khoroshev@gmail.com>
2021-09-21 10:48:30 +0300

    Решить задачу 01-02-08-02

---
+++ src/quest01/level02/lesson08/task02/Main.java
@@ -0,0 +1,15 @@
+package quest01.level02.lesson08.task02;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int int1 = scanner.nextInt();
+        int int2 = scanner.nextInt();
+        int int3 = scanner.nextInt();
+
+        System.out.println((int1 + int2 + int3) / 3);
+    }
+}
