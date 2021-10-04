83fb4048f66f899b88142865ebee415fcc4ce42b
Vadim Khoroshev <vs.khoroshev@gmail.com>
2021-10-04 11:35:13 +0300

    Решить задачу 01-03-06-03

---
+++ src/quest01/level03/lesson06/task03/Main.java
@@ -0,0 +1,21 @@
+package quest01.level03.lesson06.task03;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+
+        if (x > 0 && y > 0) {
+            System.out.println("1");
+        } else if (x < 0 && y > 0) {
+            System.out.println("2");
+        } else if (x < 0 && y < 0) {
+            System.out.println("3");
+        } else if (x > 0 && y < 0) {
+            System.out.println("4");
+        }
+    }
+}
