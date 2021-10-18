fbd5c7705794967de598171bd2cb035a53637a39
Vadim Khoroshev <vs.khoroshev@gmail.com>
2021-10-18 12:05:09 +0300

    Решить задачу 01-04-03-01

---
+++ src/quest01/level04/lesson03/task01/Main.java
@@ -0,0 +1,13 @@
+package quest01.level04.lesson03.task01;
+
+public class Main {
+    public static void main(String[] args) {
+        String quote = "Я никогда не буду работать за копейки. Амиго";
+        int number = 0;
+
+        while (number < 100) {
+            System.out.println(quote);
+            number++;
+        }
+    }
+}
