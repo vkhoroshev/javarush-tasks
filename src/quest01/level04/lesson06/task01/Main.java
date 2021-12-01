8b0e874f9b2106ed50f898bef10d4ecaea8383c3
Vadim Khoroshev <vs.khoroshev@gmail.com>
2021-12-01 16:06:03 +0300

    Решить задачу 01-04-06-01

---
+++ src/quest01/level04/lesson06/task01/Main.java
@@ -0,0 +1,11 @@
+package quest01.level04.lesson06.task01;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int number = 1; number < 15; number++) {
+            if (number % 2 == 0) {
+                System.out.println(number);
+            }
+        }
+    }
+}
