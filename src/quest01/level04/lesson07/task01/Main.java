2a60267786bc9d11faa6355f8b205f746d57429c
Vadim Khoroshev <vs.khoroshev@gmail.com>
2021-12-01 16:09:34 +0300

    Решить задачу 01-04-07-01

---
+++ src/quest01/level04/lesson07/task01/Main.java
@@ -0,0 +1,12 @@
+package quest01.level04.lesson07.task01;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("8");
+            }
+            System.out.println();
+        }
+    }
+}
