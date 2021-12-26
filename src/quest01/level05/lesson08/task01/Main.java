c883e7588ae04eefaba55bd2fad65ee78ad32a0d
Vadim Khoroshev <vs.khoroshev@gmail.com>
2021-12-26 18:55:54 +0300

    Решить задачу 01-05-08-01

---
+++ src/quest01/level05/lesson08/task01/Main.java
@@ -0,0 +1,13 @@
+package quest01.level05.lesson08.task01;
+
+public class Main {
+    public static int result = 105;
+
+    public static void main(String[] args) {
+        byte correction = Byte.MAX_VALUE;
+        for (double fahrenheit = -459.67; fahrenheit < 451; fahrenheit += 40) {
+            correction *= fahrenheit;
+            System.out.println(correction);
+        }
+    }
+}
