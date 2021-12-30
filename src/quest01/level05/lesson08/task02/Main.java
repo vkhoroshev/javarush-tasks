5fcf77a6f007d7427f70d3850ef970608c89f023
Vadim Khoroshev <vs.khoroshev@gmail.com>
2021-12-30 22:27:35 +0300

    Решить задачу 01-05-08-02

---
+++ src/quest01/level05/lesson08/task02/Main.java
@@ -0,0 +1,10 @@
+package quest01.level05.lesson08.task02;
+
+public class Main {
+    public static int result = 45967;
+
+    public static void main(String[] args) {
+        String loop = "    for (double fahrenheit = -459.67; fahrenheit < 451; fahrenheit += 40) {\n";
+        System.out.println(loop);
+    }
+}
