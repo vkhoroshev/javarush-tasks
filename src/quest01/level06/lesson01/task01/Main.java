f3b75d89adf221be34099df2ee65226777876db5
Vadim Khoroshev <vs.khoroshev@gmail.com>
2022-01-21 16:20:15 +0300

    Решить задачу 01-06-01-01

---
+++ src/quest01/level06/lesson01/task01/Main.java
@@ -0,0 +1,12 @@
+package quest01.level06.lesson01.task01;
+
+/*
+Создаем массивы
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        int[] intArray = new int[10];
+        double[] doubleArray = new double[10];
+    }
+}
