19409f4e216233047955ee38206daae6dd67bbde
Vadim Khoroshev <vs.khoroshev@gmail.com>
2022-01-22 23:13:14 +0300

    Решить задачу 01-06-01-02

---
+++ src/quest01/level06/lesson01/task02/Main.java
@@ -0,0 +1,28 @@
+package quest01.level06.lesson01.task02;
+
+/*
+Четные и нечетные ячейки массива
+*/
+
+public class Main {
+    public static final String ODD = "Нечётный";
+    public static final String EVEN = "Чётный";
+    public static String[] strings = new String[5];
+
+    public static void main(String[] args) {
+        for (int i = 0; i < strings.length; i++) {
+            strings[i] = (i % 2 == 0) ? EVEN : ODD;
+        }
+
+        System.out.print("index = 0");
+        System.out.println(" value = " + strings[0]);
+        System.out.print("index = 1");
+        System.out.println(" value = " + strings[1]);
+        System.out.print("index = 2");
+        System.out.println(" value = " + strings[2]);
+        System.out.print("index = 3");
+        System.out.println(" value = " + strings[3]);
+        System.out.print("index = 4");
+        System.out.println(" value = " + strings[4]);
+    }
+}
