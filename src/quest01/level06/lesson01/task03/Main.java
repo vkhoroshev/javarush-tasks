43f51dbc8b10e2a96258673aa049ca245631633c
Vadim Khoroshev <vs.khoroshev@gmail.com>
2022-01-26 10:57:50 +0300

    Решить задачу 01-06-01-03

---
+++ src/quest01/level06/lesson01/task03/Main.java
@@ -0,0 +1,23 @@
+package quest01.level06.lesson01.task03;
+
+/*
+Работаем с элементами массива
+*/
+
+public class Main {
+    public static int[] array = new int[]{-1, 2, 3, -4, -5};
+
+    public static void main(String[] args) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                array[i] *= -1;
+            }
+        }
+
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+        System.out.println(array[3]);
+        System.out.println(array[4]);
+    }
+}
