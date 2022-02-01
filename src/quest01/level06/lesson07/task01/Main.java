ea46e0fd0876c59837108c6790126197d42e3e5b
Vadim Khoroshev <vs.khoroshev@gmail.com>
2022-02-01 15:36:58 +0300

    Решить задачу 01-06-07-01

---
+++ src/quest01/level06/lesson07/task01/Main.java
@@ -0,0 +1,17 @@
+package quest01.level06.lesson07.task01;
+
+import java.util.Arrays;
+
+/*
+Выводим массивы
+*/
+
+public class Main {
+    public static String[] strings = new String[]{"I", "love", "Java"};
+    public static int[] ints = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(strings));
+        System.out.println(Arrays.toString(ints));
+    }
+}
