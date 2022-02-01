1f0d363671dd0126f22e98cdf44775d608f49f57
Vadim Khoroshev <vs.khoroshev@gmail.com>
2022-02-01 15:27:29 +0300

    Решить задачу 01-06-06-01

---
+++ src/quest01/level06/lesson06/task01/Main.java
@@ -0,0 +1,28 @@
+package quest01.level06.lesson06.task01;
+
+/*
+Треугольный массив
+*/
+
+public class Main {
+    public static int[][] result = new int[10][];
+
+    public static void main(String[] args) {
+        for (int i = 0; i < result.length; i++) {
+            result[i] = new int[i + 1];
+        }
+
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[i].length; j++) {
+                result[i][j] = i + j;
+            }
+        }
+
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[i].length; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
